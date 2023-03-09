29b5fdc76f155a53a2363906ce182df56de0340b
Artem <artem4rolov@gmail.com>
2023-03-09 22:44:10 +0300

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import { useState } from "react";
+import "./App.css";
+
+function App() {
+  return (
+    <div className="App">
+      <header className="header">header</header>
+      <main className="main">main</main>
+      <footer className="footer">footer</footer>
+    </div>
+  );
+}
+
+export default App;
